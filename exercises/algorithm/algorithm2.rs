4bf7d0e3e3679dc217aaae377cc05b17bc0f6fb1
Murrrphy <444575716@qq.com>
2024-10-14 20:11:29 +0800

    update

--- exercises/algorithm/algorithm2.rs
+++ exercises/algorithm/algorithm2.rs
@@ -2,7 +2,7 @@
 	double linked list reverse
 	This problem requires you to reverse a doubly linked list
 */
-// I AM NOT DONE
+
 
 use std::fmt::{self, Display, Formatter};
 use std::ptr::NonNull;
@@ -73,7 +73,15 @@ impl<T> LinkedList<T> {
         }
     }
 	pub fn reverse(&mut self){
-		// TODO
+		let mut current = self.start;
+        while let Some(mut node) = current {
+            unsafe {
+                let node_ref = node.as_mut();
+                std::mem::swap(&mut node_ref.next, &mut node_ref.prev);
+                current = node_ref.prev;
+            }
+        }
+        std::mem::swap(&mut self.start, &mut self.end);
 	}
 }
 
--- exercises/algorithm/algorithm3.rs
+++ exercises/algorithm/algorithm3.rs
@@ -3,10 +3,17 @@
 	This problem requires you to implement a sorting algorithm
 	you can use bubble sorting, insertion sorting, heap sorting, etc.
 */
-// I AM NOT DONE
 
-fn sort<T>(array: &mut [T]){
-	//TODO
+
+fn sort<T: PartialOrd>(array: &mut [T]){
+	let n = array.len();
+    for i in 0..n {
+        for j in i+1..n {
+            if array[j] < array[i] {
+                array.swap(j, i);
+            }
+        }
+    }
 }
 #[cfg(test)]
 mod tests {
